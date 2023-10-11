23d734bcaf23664ee58492d8243ef4eb432eef61
Musicalaudio <jordandsouza352@gmail.com>
2023-10-12 00:07:22 +1300

    replaced Typography objects with prop union types

--- frontend/src/typography/Typography.tsx
+++ frontend/src/typography/Typography.tsx
@@ -1,35 +1,19 @@
 import { ReactNode } from 'react';
 
-const tagMap = {
-  h1: 'h1',
-  h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-  h5: 'h5',
-  h6: 'h6',
-  p: 'p',
-};
-
-const variantMap = {
-  xl: 'heading-xl',
-  l: 'heading-l',
-  m: 'heading-m',
-  s: 'heading-s',
-  xs: 'heading-xs',
-};
-
 interface TypographyProps {
   tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-  variant: string;
+  variant: 'xl' | 'l' | 'm' | 's' | 'xs';
   children: ReactNode;
 }
 
+/* might have to refactor later to extend JSX.Instrinsic elements or something like that 
+so we can get props for things like aria-label*/
+
 const Typography = ({ tag, variant, children, ...props }: TypographyProps) => {
-  const Component = tag ? tagMap[tag as keyof Object] : 'p';
-  const variantClass = variant ? variantMap[variant as keyof Object] : '';
+  const Component = tag;
 
   return (
-    <Component className={variantClass} {...props}>
+    <Component className={`heading-${variant}`} {...props}>
       {children}
     </Component>
   );
