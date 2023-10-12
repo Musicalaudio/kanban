a0ed422948205d5c6528a017b72f0ec315ed8e50
Musicalaudio <jordandsouza352@gmail.com>
2023-10-12 22:47:32 +1300

    added font prop and class to typography component

--- frontend/src/typography/Typography.tsx
+++ frontend/src/typography/Typography.tsx
@@ -3,17 +3,24 @@ import { ReactNode } from 'react';
 interface TypographyProps {
   tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
   variant: 'xl' | 'l' | 'm' | 's' | 'xs';
+  font?: 'base' | 'accent';
   children: ReactNode;
 }
 
 /* might have to refactor later to extend JSX.Instrinsic elements or something like that 
 so we can get props for things like aria-label*/
 
-const Typography = ({ tag, variant, children, ...props }: TypographyProps) => {
+const Typography = ({
+  tag,
+  variant,
+  children,
+  font = 'base',
+  ...props
+}: TypographyProps) => {
   const Component = tag;
 
   return (
-    <Component className={`heading-${variant}`} {...props}>
+    <Component className={`heading-${variant} ff-${font}`} {...props}>
       {children}
     </Component>
   );
