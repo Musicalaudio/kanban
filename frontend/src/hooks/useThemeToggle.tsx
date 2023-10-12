4115af5a47968f9936c9499c6e16b37351e35c82
Musicalaudio <jordandsouza352@gmail.com>
2023-10-12 22:51:53 +1300

    started on useThemeToggle custom hook

---
+++ frontend/src/hooks/useThemeToggle.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+const useThemeToggle = () => {
+  const [theme, setTheme] = useState(localStorage.theme);
+  const mode = theme == 'light' ? 'dark' : 'light';
+  // return (<></>);
+};
+
+export default useThemeToggle;
