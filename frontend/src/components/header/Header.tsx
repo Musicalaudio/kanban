64f8b46d403892f8953f22b236bb2a457af1680f
Musicalaudio <jordandsouza352@gmail.com>
2023-10-13 21:06:29 +1300

    added basic layout for header component and main

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,14 +1,15 @@
 import Typography from '../../typography/Typography';
 import Button from '../button/Button';
 import VerticalEllipses from '../../../assets/icon-vertical-ellipsis.svg';
+import styles from './Header.module.scss';
 
 const Header = () => {
   return (
-    <header>
+    <header className={`flex ${styles.header}`}>
       <Typography variant="xl" tag="h1">
         Platform Launch
       </Typography>
-      <div>
+      <div className={styles.header__btns}>
         <Button>Add New Task</Button>
         <button>
           <img alt="Edit/Delete Board Dropdown" src={VerticalEllipses} />
