fc3e9499f11242873b2357f897370866663b3b9b
Musicalaudio <jordandsouza352@gmail.com>
2023-11-14 01:55:32 +1300

    put period in front of custom domain

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -42,7 +42,7 @@ export const login = async (req: express.Request, res: express.Response) => {
   await user.save();
 
   res.cookie('KANBAN-AUTH', user.authentication.sessionToken, {
-    domain: 'kanban-odr9.onrender.com',
+    domain: '.kanban-odr9.onrender.com',
     path: '/',
     sameSite: 'none',
     secure: true,
