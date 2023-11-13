45e7e108169a2299e4f99edba29ed04519a14c9b
Musicalaudio <jordandsouza352@gmail.com>
2023-11-13 19:13:47 +1300

    used vercel domain in cookie

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -42,7 +42,7 @@ export const login = async (req: express.Request, res: express.Response) => {
   await user.save();
 
   res.cookie('KANBAN-AUTH', user.authentication.sessionToken, {
-    domain: '.kanban-musicalaudio.netlify.app',
+    domain: 'kanban-client-musicalaudio.vercel.app',
     path: '/',
     sameSite: 'none',
     secure: true,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,34 +19,6 @@ const port = process.env.PORT || 8050;
 const app = express();
 app.set('trust proxy', 1);
 //middleware
-// if(proc)
-
-// const corsTest = app.use((req, res, next) => {
-//   //set header first to allow request or origin domain (value can be different)
-//   console.log('HELLO MAAAAAAAAAAAAAAN');
-//   res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN as string);
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PUT, PATCH, OPTIONS, DELETE'
-//   );
-
-//   //---- other code
-//   console.log(req.method);
-//   //Preflight CORS handler
-//   if (req.method === 'OPTIONS') {
-//     console.log('YUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUH');
-//     return res.status(200).json({
-//       body: 'OK',
-//     });
-//   }
-//   next();
-// });
-
 app.use(
   cors({
     origin: process.env.ORIGIN as string,
