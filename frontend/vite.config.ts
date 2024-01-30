4c4e7bd4d2acd8f0de1247812ad91b99f2187f18
Musicalaudio <62318170+Musicalaudio@users.noreply.github.com>
2024-01-31 00:06:38 +1300

    Update vite.config.ts
    Testing changes to vite config file in production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,11 +7,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8050',
-    },
-  },
   plugins: [react()],
   test: {
     globals: true,
